fa6b7f76ae31eef4d821dff2ca7851d2b2bfec85
mijonOfTheMoon <devopshasbi@gmail.com>
2024-11-29 15:19:41 +0700

    fix bug

--- app/Http/Controllers/API/EventController.php
+++ app/Http/Controllers/API/EventController.php
@@ -7,18 +7,19 @@ use Illuminate\Http\Request;
 use App\Models\Event;
 use App\Models\Category;
 use App\Models\Registration;
+use Aws\S3\S3Client;
 
 class EventController extends Controller
 {
 
-    public $s3 = new S3Client([
-        'version' => 'latest',
-        'region'  => env('AWS_DEFAULT_REGION'),
-        'credentials' => [
-            'key'    => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        ],
-    ]);
+    // public $s3 = new S3Client([
+    //     'version' => 'latest',
+    //     'region'  => env('AWS_DEFAULT_REGION'),
+    //     'credentials' => [
+    //         'key'    => env('AWS_ACCESS_KEY_ID'),
+    //         'secret' => env('AWS_SECRET_ACCESS_KEY'),
+    //     ],
+    // ]);
 
     public function index(Request $request)
     {
