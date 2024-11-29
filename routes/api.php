2e2fef185f4b2a616dc1756e5826797acc10e9fb
mijonOfTheMoon <devopshasbi@gmail.com>
2024-11-29 15:14:00 +0700

    rollback

--- routes/api.php
+++ routes/api.php
@@ -1,52 +1,5 @@
 <?php
 
-// use Illuminate\Support\Facades\Route;
-// use App\Http\Controllers\API\AuthController;
-// use App\Http\Controllers\API\UserController;
-// use App\Http\Controllers\API\EventController;
-
-// Route::post('/register', [AuthController::class, 'register']);
-
-// Route::post('/login', [AuthController::class, 'login']);
-
-// Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
-
-// Route::post('/user', [UserController::class, 'update'])->middleware('auth:sanctum');
-
-// Route::get('/user', [UserController::class, 'read'])->middleware('auth:sanctum');
-
-// Route::delete('/user', [UserController::class, 'delete'])->middleware('auth:sanctum');
-
-// Route::post('/change-password', [UserController::class, 'change'])->middleware('auth:sanctum');
-
-// Route::get('/events', [EventController::class, 'index']);
-
-// Route::get('/events/{id}/view', [EventController::class, 'details']);
-
-// Route::get('/categories', [EventController::class, 'categories']);
-
-// Route::get('/my-events/all', [EventController::class, 'mine'])->middleware('auth:sanctum');
-
-// Route::get('/my-events/registered', [EventController::class, 'registered'])->middleware('auth:sanctum');
-
-// Route::get('/my-events/cancelled', [EventController::class, 'cancelled'])->middleware('auth:sanctum');
-
-// Route::get('/my-events/{id}', [EventController::class, 'filter'])->middleware('auth:sanctum');
-
-// Route::get('/events/{id}/participants/all', [EventController::class, 'participants'])->middleware('auth:sanctum');
-
-// Route::get('/events/{id}/participants/cancelled', [EventController::class, 'absent'])->middleware('auth:sanctum');
-
-// Route::get('/events/{id}/participants/registered', [EventController::class, 'attend'])->middleware('auth:sanctum');
-
-// Route::get('/events/{id}/kode-unik', [EventController::class, 'kode'])->middleware('auth:sanctum');
-
-// Route::post('/events/{id}/register', [EventController::class, 'register'])->middleware('auth:sanctum');
-
-// Route::post('/events/{id}/cancel', [EventController::class, 'cancel'])->middleware('auth:sanctum');
-
-// Route::post('/events', [EventController::class, 'create'])->middleware('auth:sanctum');
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\UserController;
@@ -56,15 +9,15 @@ Route::post('/register', [AuthController::class, 'register']);
 
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::post('/logout', [AuthController::class, 'logout']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::post('/user', [UserController::class, 'update']);
+Route::post('/user', [UserController::class, 'update'])->middleware('auth:sanctum');
 
-Route::get('/user', [UserController::class, 'read']);
+Route::get('/user', [UserController::class, 'read'])->middleware('auth:sanctum');
 
-Route::delete('/user', [UserController::class, 'delete']);
+Route::delete('/user', [UserController::class, 'delete'])->middleware('auth:sanctum');
 
-Route::post('/change-password', [UserController::class, 'change']);
+Route::post('/change-password', [UserController::class, 'change'])->middleware('auth:sanctum');
 
 Route::get('/events', [EventController::class, 'index']);
 
@@ -72,24 +25,24 @@ Route::get('/events/{id}/view', [EventController::class, 'details']);
 
 Route::get('/categories', [EventController::class, 'categories']);
 
-Route::get('/my-events/all', [EventController::class, 'mine']);
+Route::get('/my-events/all', [EventController::class, 'mine'])->middleware('auth:sanctum');
 
-Route::get('/my-events/registered', [EventController::class, 'registered']);
+Route::get('/my-events/registered', [EventController::class, 'registered'])->middleware('auth:sanctum');
 
-Route::get('/my-events/cancelled', [EventController::class, 'cancelled']);
+Route::get('/my-events/cancelled', [EventController::class, 'cancelled'])->middleware('auth:sanctum');
 
-Route::get('/my-events/{id}', [EventController::class, 'filter']);
+Route::get('/my-events/{id}', [EventController::class, 'filter'])->middleware('auth:sanctum');
 
-Route::get('/events/{id}/participants/all', [EventController::class, 'participants']);
+Route::get('/events/{id}/participants/all', [EventController::class, 'participants'])->middleware('auth:sanctum');
 
-Route::get('/events/{id}/participants/cancelled', [EventController::class, 'absent']);
+Route::get('/events/{id}/participants/cancelled', [EventController::class, 'absent'])->middleware('auth:sanctum');
 
-Route::get('/events/{id}/participants/registered', [EventController::class, 'attend']);
+Route::get('/events/{id}/participants/registered', [EventController::class, 'attend'])->middleware('auth:sanctum');
 
-Route::get('/events/{id}/kode-unik', [EventController::class, 'kode']);
+Route::get('/events/{id}/kode-unik', [EventController::class, 'kode'])->middleware('auth:sanctum');
 
-Route::post('/events/{id}/register', [EventController::class, 'register']);
+Route::post('/events/{id}/register', [EventController::class, 'register'])->middleware('auth:sanctum');
 
-Route::post('/events/{id}/cancel', [EventController::class, 'cancel']);
+Route::post('/events/{id}/cancel', [EventController::class, 'cancel'])->middleware('auth:sanctum');
 
-Route::post('/events', [EventController::class, 'create']);
+Route::post('/events', [EventController::class, 'create'])->middleware('auth:sanctum');
