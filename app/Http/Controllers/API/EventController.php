d2ecad2332c1d186ba4a8ab3ae4e23651af453ac
mijonOfTheMoon <devopshasbi@gmail.com>
2024-12-06 07:58:24 +0700

    more fixing

--- app/Http/Controllers/API/EventController.php
+++ app/Http/Controllers/API/EventController.php
@@ -429,7 +429,7 @@ class EventController extends Controller
             ], 401);
         }
 
-        if ($event->date < date('Y-m-d') && $event->end_time < date('H:i:s')) {
+        if ($event->date <= date('Y-m-d') && $event->end_time <= date('H:i:s')) {
             return response([
                 'message' => 'Event has passed'
             ], 400);
